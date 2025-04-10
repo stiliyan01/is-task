a430f8134de81ad00bf9b169b2e587538d3b1e5c
stiliyan <stiliyan_2001@abv.bg>
2025-04-10 16:41:01 +0300

    Fix cors problem

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 
-export default axios.create({
+const csrfToken = document.head.querySelector(
+  'meta[name="csrf-token"]'
+)?.content;
+
+const axiosInstance = axios.create({
   baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
+    "X-CSRF-TOKEN": csrfToken,
   },
 });
+
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
