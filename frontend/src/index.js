c668b5c6c5f0cf6a6dfc25e628b8f940ca7ee802
stiliyan <stiliyan_2001@abv.bg>
2025-04-10 16:46:50 +0300

    Add routes to the router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,14 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
 import Checkout from "./pages/Checkout";
 import AdminPanel from "./pages/admin/AdminPanel";
-import ProductPage from "./pages/admin/ProductPage";
-import CategoryPage from "./pages/admin/CategoryPage";
+
+import ProductPage from "./pages/admin/product/ProductPage";
+import CreateProductPage from "./pages/admin/product/CreateProductPage";
+import EditProductPage from "./pages/admin/product/EditProductPage";
+
+import CategoryPage from "./pages/admin/category/CategoryPage";
+import CreateCategoryPage from "./pages/admin/category/CreateCategoryPage";
+import EditCategoryPage from "./pages/admin/category/EditCategoryPage";
 
 const router = createBrowserRouter([
   {
@@ -28,8 +34,25 @@ const router = createBrowserRouter([
         element: <ProductPage />,
       },
       {
+        path: "/admin/products/:id",
+        element: <EditProductPage />,
+      },
+      {
+        path: "/admin/products/create",
+        element: <CreateProductPage />,
+      },
+
+      {
         path: "/admin/categories",
         element: <CategoryPage />,
+      },
+      {
+        path: "/admin/categories/create",
+        element: <CreateCategoryPage />,
+      },
+      {
+        path: "/admin/categories/:id",
+        element: <EditCategoryPage />,
       },
     ],
   },
