fe76cc4086ee1b73f5610d721e22f1e0e88e921e
stiliyan <stiliyan_2001@abv.bg>
2025-04-13 15:21:13 +0300

    Add checks is admin and better redirecting

--- frontend/src/components/admin/TableRow.jsx
+++ frontend/src/components/admin/TableRow.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Pencil, Trash2, Eye } from "lucide-react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const TableRow = React.memo(
   ({ item, columns, onDelete, textForLink, isForDetails = false }) => {
     const user = JSON.parse(sessionStorage.getItem("user"));
+    const isAdmin = String(user?.is_admin) === "1";
+    const location = useLocation();
+
+    const isInProfile = location.pathname.includes("/profile");
+
+    const detailsLink = isInProfile
+      ? `/profile/${textForLink}/${item.id}`
+      : `/admin/${textForLink}/${item.id}`;
+
+    const editLink = `/admin/${textForLink}/${item.id}`;
+
     return (
       <tr className="border-t">
         {columns.map((col) => (
@@ -14,14 +25,18 @@ const TableRow = React.memo(
         ))}
         <td className="px-4 py-2 text-right">
           <div className="flex justify-end space-x-4">
-            <NavLink to={`/admin/${textForLink}/${item.id}`}>
-              {isForDetails ? (
+            {isForDetails ? (
+              <NavLink to={detailsLink}>
                 <Eye className="w-4 h-4 text-green-600 cursor-pointer" />
-              ) : (
-                <Pencil className="w-4 h-4 text-yellow-600 cursor-pointer" />
-              )}
-            </NavLink>
-            {user.is_admin === 1 && (
+              </NavLink>
+            ) : (
+              isAdmin && (
+                <NavLink to={editLink}>
+                  <Pencil className="w-4 h-4 text-yellow-600 cursor-pointer" />
+                </NavLink>
+              )
+            )}
+            {isAdmin && (
               <Trash2
                 className="w-4 h-4 text-red-600 cursor-pointer"
                 onClick={() => onDelete(item.id)}
